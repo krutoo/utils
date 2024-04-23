78b72bf8b148602db9414a23e5c6bbe792b81f3d
krutoo <dima.krutoo@yandex.ru>
2024-04-23 16:28:35 +0500

    new utils and JSDoc
    - fix slow types

--- src/lang/wait.ts
+++ src/lang/wait.ts
@@ -3,6 +3,6 @@
  * @param ms Timeout in milliseconds.
  * @returns Promise.
  */
-export function wait(ms: number) {
+export function wait(ms: number): Promise<void> {
   return new Promise((done) => setTimeout(done, ms));
 }
