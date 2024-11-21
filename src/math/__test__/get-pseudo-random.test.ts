b918a143f3f7d022e9424834c8aa88ca76a56e6e
krutoo <dima.krutoo@yandex.ru>
2024-11-21 18:36:54 +0500

    getPseudoRandom
    - `math`: function `getPseudoRandom` added

---
+++ src/math/__test__/get-pseudo-random.test.ts
@@ -0,0 +1,36 @@
+import { test } from '@std/testing/bdd';
+import { expect } from '@std/expect';
+import { getPseudoRandom } from '../get-pseudo-random.ts';
+
+// maybe this is working differently depends on JS runtime
+test('value check', () => {
+    const random = getPseudoRandom(0);
+
+    expect(random()).toBe(0.21132115912208504);
+    expect(random()).toBe(0.7094221536351166);
+    expect(random()).toBe(0.5467721193415638);
+});
+
+test('result value should be in range 0-1', () => {
+    const random = getPseudoRandom(Math.random() * 100);
+
+    for (let i = 0; i < 1000; i++) {
+        const value = random();
+        expect(value >= 0).toBe(true);
+        expect(value <= 1).toBe(true);
+    }
+});
+
+test('generators that based on same seed should return same values', () => {
+    const first = getPseudoRandom(123);
+    const second = getPseudoRandom(123);
+
+    for (let i = 0; i < 100; i++) {
+        const a = first();
+        const b = second();
+
+        expect(typeof a === 'number').toBe(true);
+        expect(typeof b === 'number').toBe(true);
+        expect(a).toBe(b);
+    }
+});
