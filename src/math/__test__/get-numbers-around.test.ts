65e36567659b10f364b5e171ce8342b482baa39b
krutoo <dima.krutoo@yandex.ru>
2024-11-25 13:46:24 +0500

    getNumbersAround
    - `math`: function `getNumbersAround` added, useful for creating pagination buttons in UI

---
+++ src/math/__test__/get-numbers-around.test.ts
@@ -0,0 +1,18 @@
+import { test } from '@std/testing/bdd';
+import { expect } from '@std/expect';
+import { getNumbersAround } from '../get-numbers-around.ts';
+
+test('should work with defaults', () => {
+  expect(getNumbersAround(0)).toEqual([0, 1]);
+  expect(getNumbersAround(1)).toEqual([0, 1, 2]);
+  expect(getNumbersAround(2)).toEqual([1, 2, 3]);
+  expect(getNumbersAround(10)).toEqual([9, 10, 11]);
+  expect(getNumbersAround(54)).toEqual([53, 54, 55]);
+});
+
+test('should handle options', () => {
+  expect(getNumbersAround(0, { min: -10, range: 3 })).toEqual([-3, -2, -1, 0, 1, 2, 3]);
+  expect(getNumbersAround(3, { range: 3 })).toEqual([0, 1, 2, 3, 4, 5, 6]);
+  expect(getNumbersAround(97, { range: 2, max: 98 })).toEqual([95, 96, 97, 98]);
+  expect(getNumbersAround(99, { range: 2, max: 100 })).toEqual([97, 98, 99, 100]);
+});
---
+++ src/math/get-numbers-around.ts
@@ -0,0 +1,45 @@
+export interface GetNumbersAroundOptions {
+  /** Range size. For range = 2 and target = 3 result will be [1, 2, 3, 4, 5]. */
+  range?: number;
+
+  /** Bottom limit of range. */
+  min?: number;
+
+  /** Top limit of range. */
+  max?: number;
+}
+
+/**
+ * Returns sorted list of numbers around target number, including target number.
+ * Useful for making pagination buttons in UI.
+ * @param target Target value.
+ * @param options Options of borders and range of result set of numbers.
+ * @returns Array of sorted numbers.
+ * @example
+ * ```ts
+ * import { getNumbersAround } from "@krutoo/utils";
+ *
+ * getNumbersAround(5); // [4, 5, 6]
+ *
+ * getNumbersAround(5, { range: 2 }); // [3, 4, 5, 6, 7]
+ *
+ * getNumbersAround(1, { range: 2 }); // [0, 1, 2, 3]
+ *
+ * getNumbersAround(1, { min: -1, range: 2 }); // [-1, 0, 1, 2, 3]
+ *
+ * getNumbersAround(99, { range: 2, max: 100 }); // [97, 98, 99, 100]
+ * ```
+ */
+export function getNumbersAround(target: number, {
+  range = 1,
+  min = 0,
+  max = Infinity,
+}: GetNumbersAroundOptions = {}): number[] {
+  const numbers: number[] = [];
+
+  for (let i = Math.max(min, target - range); i <= Math.min(max, target + range); i++) {
+    numbers.push(i);
+  }
+
+  return numbers;
+}
