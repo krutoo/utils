22d82cab9fdfcef69f1fa0f2733d416d0d52d009
krutoo <dima.krutoo@yandex.ru>
2024-12-12 21:14:15 +0500

    some react tests added

---
+++ tests/src/use-debounce-state.test.tsx
@@ -0,0 +1,52 @@
+import assert from 'node:assert/strict';
+import { describe, test } from 'node:test';
+import { fireEvent, render } from '@testing-library/react';
+import { useState } from 'react';
+import { useDebounceState } from '@krutoo/utils/react';
+import { wait } from '@krutoo/utils';
+
+function TestComponent() {
+  const [value, setValue] = useState('');
+  const debouncedValue = useDebounceState(value, 200);
+
+  const handleChange = (event: { target: { value: string } }) => {
+    setValue(event.target.value);
+  };
+
+  return (
+    <>
+      <input data-testid='field' value={value} onChange={handleChange} />
+      <span data-testid='caption'>{debouncedValue}</span>
+    </>
+  );
+}
+
+describe('useDebounceState', () => {
+  test('Should return debounce state', async () => {
+    const { getByTestId } = render(<TestComponent />);
+    const field = getByTestId('field') as HTMLInputElement;
+    const caption = getByTestId('caption');
+
+    // initial state
+    assert.equal(field.value, '');
+    assert.equal(caption.textContent, '');
+
+    // change input
+    fireEvent.change(field, { target: { value: 'something' } });
+    assert.equal(field.value, 'something');
+    assert.equal(caption.textContent, '');
+
+    // wait for debounced state updates
+    await wait(250);
+    assert.equal(caption.textContent, 'something');
+
+    // change input again
+    fireEvent.change(field, { target: { value: 'foobar' } });
+    assert.equal(field.value, 'foobar');
+    assert.equal(caption.textContent, 'something');
+
+    // wait for debounced state updates
+    await wait(250);
+    assert.equal(caption.textContent, 'foobar');
+  });
+});
---
+++ tests/src/use-identity-ref.test.tsx
@@ -0,0 +1,46 @@
+import assert from 'node:assert/strict';
+import { test, describe } from 'node:test';
+import { render } from '@testing-library/react';
+import { useIdentityRef } from '@krutoo/utils/react';
+import { useEffect } from 'react';
+
+interface TestComponentProps {
+  value: number;
+  onRefChange?: VoidFunction;
+}
+
+function TestComponent({ value, onRefChange }: TestComponentProps) {
+  const ref = useIdentityRef<number>(value);
+
+  useEffect(() => {
+    onRefChange?.();
+  }, [ref, onRefChange]);
+
+  return <div data-testid='display'>Ref value: {ref.current}</div>;
+}
+
+describe('useIdentityRef', () => {
+  test('should return stable ref with actual value', () => {
+    let refChangeCount = 0;
+
+    const handleRefChange = () => {
+      refChangeCount++;
+    };
+
+    const { getByTestId, rerender } = render(
+      <TestComponent value={1} onRefChange={handleRefChange} />,
+    );
+
+    const display = getByTestId('display');
+    assert.equal(display.textContent, 'Ref value: 1');
+    assert.equal(1, refChangeCount);
+
+    rerender(<TestComponent value={12} onRefChange={handleRefChange} />);
+    assert.equal(display.textContent, 'Ref value: 12');
+    assert.equal(1, refChangeCount);
+
+    rerender(<TestComponent value={123} onRefChange={handleRefChange} />);
+    assert.equal(display.textContent, 'Ref value: 123');
+    assert.equal(1, refChangeCount);
+  });
+});
