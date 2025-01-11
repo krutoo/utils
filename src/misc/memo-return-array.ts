00a6f0cbcc67a5820849966129234096c885a4b0
krutoo <dima.krutoo@yandex.ru>
2025-01-11 19:48:11 +0500

    memoReturnArray added

---
+++ src/misc/memo-return-array.ts
@@ -0,0 +1,52 @@
+const stub = Symbol();
+
+/** Options for `memoReturnArray`. */
+export interface MemoReturnArrayOptions {
+  /** Equality checker. */
+  isEqual?: (a: any, b: any) => boolean;
+}
+
+/**
+ * Decorates given function (returns new function).
+ * Result function memoizes array returned from it.
+ * If all array items are same, previous call result will be returned.
+ *
+ * Useful in Redux and Reselect when your selector returns new array with same items.
+ * @param func Function.
+ * @param options Options.
+ * @returns Function.
+ */
+export function memoReturnArray<T extends (...args: any[]) => any[]>(
+  func: T,
+  { isEqual = Object.is }: MemoReturnArrayOptions = {},
+): (...args: Parameters<T>) => ReturnType<T> {
+  let lastResult: typeof stub | ReturnType<T> = stub;
+
+  return (...args: Parameters<T>): ReturnType<T> => {
+    if (lastResult === stub) {
+      const result = func(...args) as ReturnType<T>;
+
+      lastResult = result;
+
+      return result;
+    }
+
+    const result = func(...args) as ReturnType<T>;
+
+    if (result.length !== lastResult.length) {
+      lastResult = result;
+
+      return result;
+    }
+
+    for (let i = 0; i < result.length; i++) {
+      if (!isEqual(lastResult[i], result[i])) {
+        lastResult = result;
+
+        return result;
+      }
+    }
+
+    return lastResult;
+  };
+}
--- src/misc/mod.ts
+++ src/misc/mod.ts
@@ -1,3 +1,4 @@
 export * from './for-each-pair.ts';
+export * from './memo-return-array.ts';
 export * from './pairs.ts';
 export * from './plural.ts';
