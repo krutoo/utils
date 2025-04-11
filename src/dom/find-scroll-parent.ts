250d22c478ac6fd338bf6d30c0fea040fb16f2f1
krutoo <dima.krutoo@yandex.ru>
2025-04-11 16:45:27 +0500

    DOM utils upd
    - dom: function `findAncestor` added (minor)
    - dom: findScrollParent now is a combination of findAncestor and isScrollable (patch)
    - dom: getPositionedParentOffset now uses findAncestor and isScrollable (patch)

--- src/dom/find-scroll-parent.ts
+++ src/dom/find-scroll-parent.ts
@@ -1,16 +1,11 @@
+import { findAncestor } from './find-ancestor.ts';
 import { isScrollable } from './is-scrollable.ts';
 
 /**
- * Returns closest scrollable parent.
+ * Returns closest scrollable ancestor or null if there is no scrollable ancestors.
  * @param element Target element.
  * @returns Closest scrollable parent.
  */
 export function findScrollParent(element: Element): HTMLElement | null {
-  let parent: HTMLElement | null = element.parentElement;
-
-  while (parent && !isScrollable(parent)) {
-    parent = parent.parentElement;
-  }
-
-  return parent;
+  return findAncestor(element, isScrollable);
 }
