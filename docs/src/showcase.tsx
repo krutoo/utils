01de330980e402168617e807e3dc6798a800f967
krutoo <dima.krutoo@yandex.ru>
2025-04-20 20:13:48 +0500

    Docs fixes
    - external links now opens in new tab

--- docs/src/showcase.tsx
+++ docs/src/showcase.tsx
@@ -3,14 +3,16 @@ import { filterValidStories } from '@krutoo/showcase/runtime';
 import { ShowcaseApp } from '@krutoo/showcase/runtime-showcase';
 import { MDXProvider } from '@mdx-js/react';
 import foundStories from '#found-stories';
+import { CodeBlock } from './components/code-block/code-block.tsx';
+import { Link } from './components/link/link.tsx';
 import './reset.css';
 import '@krutoo/showcase/runtime-showcase/styles.css';
-import { CodeBlock } from './components/code-block/code-block.tsx';
 
 const { validStories } = filterValidStories(foundStories);
 
 const components = {
   pre: CodeBlock,
+  a: Link,
 };
 
 createRoot(document.getElementById('root')!).render(
