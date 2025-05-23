f1811113c1a486154ad3d690755f345de7fd2472
krutoo <dima.krutoo@yandex.ru>
2025-05-23 20:59:38 +0500

    docs: some docs improvements

--- docs/rspack.config.ts
+++ docs/rspack.config.ts
@@ -1,6 +1,6 @@
 import path from 'node:path';
 import type { Configuration } from '@rspack/core';
-import * as plugins from '@krutoo/utils/rspack';
+import * as utils from '@krutoo/utils/rspack';
 import { aliasesToSource, pluginStoriesEntry } from './.rspack/utils.ts';
 
 const PKG_IMPL = process.env.PKG_IMPL ?? 'src';
@@ -39,22 +39,22 @@ export default {
       storiesRootDir: './docs/',
       rawImport: mod => ({ importPath: `!${mod.importPath}?raw` }),
     }),
-    plugins.pluginTypeScript({
+    utils.pluginTypeScript({
       tsConfig: PKG_IMPL === 'tarball' ? false : undefined,
     }),
-    plugins.pluginCSS(),
-    plugins.pluginRawImport(),
-    plugins.pluginHTML({
+    utils.pluginCSS(),
+    utils.pluginRawImport(),
+    utils.pluginHTML({
       filename: 'index.html',
       template: './src/index.html',
       chunks: ['showcase'],
     }),
-    plugins.pluginHTML({
+    utils.pluginHTML({
       filename: 'sandbox.html',
       template: './src/index.html',
       chunks: ['sandbox'],
     }),
-    plugins.pluginPublicFiles(),
+    utils.pluginPublicFiles(),
   ],
   experiments: {
     css: false,
