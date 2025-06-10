6de97bf9c32f030b1f8d1adced8f6c4ede1d4a55
Dmitry Petrov <dima.krutoo@yandex.ru>
2025-06-10 18:36:56 +0500

    feat(math): shuffle is mutable now, toShuffled added (immutable) (#98)
    BREAKING CHANGE: shuffle now is mutates given array, use toShuffled instead

--- src/math/__test__/shuffle.test.ts
+++ src/math/__test__/shuffle.test.ts
@@ -1,12 +1,40 @@
 import { describe, test } from 'node:test';
 import { expect } from '@std/expect/expect';
-import { shuffle } from '../shuffle.ts';
+import { shuffle, toShuffled } from '../shuffle.ts';
 
 describe('shuffle', () => {
   test('should randomly sort items', () => {
-    const input = [1, 2, 3, 4, 5, 6, 7];
+    const refer = [1, 2, 3, 4, 5, 6, 7];
+    const input = [...refer];
+
     const output = shuffle(input);
 
+    expect(input === output).toBe(true);
+    let matchCount = 0;
+    for (let i = 0; i < input.length; i++) {
+      if (refer[i] === output[i]) {
+        matchCount++;
+      }
+    }
+
+    expect(matchCount < 7).toBe(true);
+  });
+});
+
+describe('toShuffled', () => {
+  test('should return randomly sorted copy', () => {
+    const refer = [1, 2, 3, 4, 5, 6, 7];
+
+    const input = [...refer];
+    const output = toShuffled(input);
+
+    // check that input is not mutated
+    for (let i = 0; i < refer.length; i++) {
+      expect(input[i] === refer[i]).toBe(true);
+    }
+
+    // check that output is new array but with same items
+    expect(output === input).toBe(false);
     expect(output.length === input.length).toBe(true);
     expect(output.every(item => input.includes(item))).toBe(true);
     expect(output.some((item, index) => input[index] !== item)).toBe(true);
--- src/math/shuffle.ts
+++ src/math/shuffle.ts
@@ -1,3 +1,27 @@
+export interface ShuffleOptions {
+  random?: () => number;
+}
+
+/**
+ * Randomly sorts items in given array.
+ * @param list Source array.
+ * @param options Options.
+ * @returns Exactly given array.
+ * @see https://stackoverflow.com/a/12646864
+ */
+export function shuffle<T extends any[]>(
+  list: T,
+  { random = Math.random }: ShuffleOptions = {},
+): T {
+  for (let i = list.length - 1; i > 0; i--) {
+    const j = Math.floor(random() * (i + 1));
+
+    [list[i] as any, list[j] as any] = [list[j], list[i]];
+  }
+
+  return list;
+}
+
 /**
  * Returns new array with random sorted items.
  * @param list Source array.
@@ -5,21 +29,6 @@
  * @returns New randomly sorted array.
  * @see https://stackoverflow.com/a/12646864
  */
-export function shuffle<T>(
-  list: Array<T>,
-  {
-    random = Math.random,
-  }: {
-    random?: () => number;
-  } = {},
-): Array<T> {
-  const result = [...list];
-
-  for (let i = result.length - 1; i > 0; i--) {
-    const j = Math.floor(random() * (i + 1));
-
-    [result[i] as any, result[j] as any] = [result[j], result[i]];
-  }
-
-  return result;
+export function toShuffled<T>(list: Array<T>, options: ShuffleOptions = {}): Array<T> {
+  return shuffle([...list], options);
 }
