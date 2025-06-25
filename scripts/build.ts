99b816d24ca1adf659aa953cc9d8a7f954eeaefc
krutoo <dima.krutoo@yandex.ru>
2025-06-25 19:22:50 +0500

    build: format js files in dist by prettier during build
    Need to reduce package size

--- scripts/build.ts
+++ scripts/build.ts
@@ -16,6 +16,9 @@ await execAsync('npx tsc -p tsconfig.build-esm.json');
 // build cjs files
 await execAsync('npx tsc -p tsconfig.build-cjs.json');
 
+// format (for reduce package size by replacing indent from 4 to 2)
+await execAsync('npx prettier "dist/**/*.js" --write --ignore-path=./.nonexistent');
+
 // add inner package.json files with type for esm and cjs
 await fs.writeFile('./dist/esm/package.json', JSON.stringify({ type: 'module' }, null, 2));
 await fs.writeFile('./dist/cjs/package.json', JSON.stringify({ type: 'commonjs' }, null, 2));
