f375525a3a545d4b3a5f913623de06bfbf95b966
Dmitry Petrov <dima.krutoo@yandex.ru>
2025-07-04 19:08:08 +0500

    feat(react): useBoundingClientRect is deprecated (#115)
    For observing element' size hook `useResize` can be used
    Also comment about missing `/testing` module export from package root

--- src/mod.ts
+++ src/mod.ts
@@ -1,5 +1,6 @@
 // IMPORTANT: "./react/mod.ts" is not added here because it requires "react" to be installed as a dependency
 // IMPORTANT: "./rspack/mod.ts" is not added here by the same reason
+// IMPORTANT: "./testing/mod.ts" is not added here because in practice it need only in test environment
 export * from './dom/mod.ts';
 export * from './math/mod.ts';
 export * from './types/mod.ts';
