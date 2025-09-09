e5d9fd156709b0ae319a37421ab880c0a405d9c6
Dmitry Petrov <dima.krutoo@yandex.ru>
2025-09-09 20:27:34 +0500

    feat(react): useStorageItem: defaultValue, storage function handle fix (#145)
    Now you can pass default value to useStorageItem, also behavior fixed for inline function pass to storage option

--- tests-e2e/tests/use-storage-item.spec.ts
+++ tests-e2e/tests/use-storage-item.spec.ts
@@ -2,18 +2,20 @@ import { test, expect } from '@playwright/test';
 
 const targetText = 'Fill value and then reload page to see that value is saved';
 
-test('useStorageItem', async ({ page }) => {
-  await page.goto('/sandbox.html?path=/use-storage-item/primary');
+test.describe('useStorageItem', () => {
+  test('should works with localStorage', async ({ page }) => {
+    await page.goto('/sandbox.html?path=/use-storage-item/primary');
 
-  const message = page.locator('p');
-  const input = page.getByTestId('input');
-  expect(await message.textContent()).toBe(targetText);
-  expect(await input.inputValue()).toBe('');
+    const message = page.locator('p');
+    const input = page.getByTestId('input');
+    expect(await message.textContent()).toBe(targetText);
+    expect(await input.inputValue()).toBe('');
 
-  await input.focus();
-  await input.fill('Foo, Bar!');
-  expect(await input.inputValue()).toBe('Foo, Bar!');
+    await input.focus();
+    await input.fill('Foo, Bar!');
+    expect(await input.inputValue()).toBe('Foo, Bar!');
 
-  await page.reload();
-  expect(await input.inputValue()).toBe('Foo, Bar!');
+    await page.reload();
+    expect(await input.inputValue()).toBe('Foo, Bar!');
+  });
 });
