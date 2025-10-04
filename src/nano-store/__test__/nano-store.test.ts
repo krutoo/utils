9ec2fa5b6251b0f96333b70ee7362a80b4930348
krutoo <dima.krutoo@yandex.ru>
2025-10-04 21:04:10 +0500

    test: Nano store tests added

---
+++ src/nano-store/__test__/nano-store.test.ts
@@ -0,0 +1,41 @@
+import { test, describe, mock } from 'node:test';
+import { expect } from '@std/expect';
+import { createStore } from '../nano-store.ts';
+
+describe('createNanoStore', () => {
+  test('should create working store', () => {
+    const store = createStore<number>(1);
+    const spy = mock.fn();
+
+    // initial value
+    expect(store.get()).toBe(1);
+
+    // value changes
+    store.set(15);
+    expect(store.get()).toBe(15);
+
+    // value changes again
+    store.set(9000);
+    expect(store.get()).toBe(9000);
+
+    // subscribe to value change
+    const unsubscribe = store.subscribe(spy);
+
+    // no calls after subscribe
+    expect(spy.mock.callCount()).toBe(0);
+
+    // no calls after change value to exact same
+    store.set(9000);
+    expect(spy.mock.callCount()).toBe(0);
+
+    // calls when value changes
+    store.set(-20);
+    expect(spy.mock.callCount()).toBe(1);
+
+    // no calls after unsubscribe
+    unsubscribe();
+    expect(spy.mock.callCount()).toBe(1);
+    store.set(-100);
+    expect(spy.mock.callCount()).toBe(1);
+  });
+});
