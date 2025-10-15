9ea042ee27d8e06ed8a6c36cd8609f4e7f01e711
krutoo <dima.krutoo@yandex.ru>
2025-10-15 18:52:39 +0500

    feat(misc): wait() can be aborted by AbortController

--- src/misc/__test__/wait.test.ts
+++ src/misc/__test__/wait.test.ts
@@ -1,4 +1,4 @@
-import { describe, test } from 'node:test';
+import { describe, mock, test } from 'node:test';
 import { expect } from '@std/expect/expect';
 import { wait } from '../wait.ts';
 
@@ -12,5 +12,25 @@ describe('wait', () => {
 
     const passed = finish - start;
     expect(passed >= timeout).toBe(true);
+  });
+
+  test('should handle abort signal', async () => {
+    const spy = mock.fn();
+    const controller = new AbortController();
+
+    wait(20000, { signal: controller.signal })
+      .then(() => {
+        spy('resolved');
+      })
+      .catch(reason => {
+        spy(`rejected, reason: ${reason}`);
+      });
+
+    controller.abort('test');
+    await Promise.resolve();
+    await Promise.resolve();
+
+    expect(spy.mock.callCount()).toBe(1);
+    expect(spy.mock.calls[0]?.arguments[0]).toBe('rejected, reason: test');
   });
 });
--- src/misc/wait.ts
+++ src/misc/wait.ts
@@ -1,8 +1,31 @@
+export interface WaitOptions {
+  /** Abort signal. */
+  signal?: AbortSignal;
+}
+
 /**
  * Returns promise that resolves after specified timeout.
  * @param ms Timeout in milliseconds.
+ * @param options Options.
  * @returns Promise.
  */
-export function wait(ms: number): Promise<void> {
-  return new Promise(done => setTimeout(done, ms));
+export function wait(ms: number, options: WaitOptions = {}): Promise<void> {
+  const { signal } = options;
+
+  if (signal?.aborted) {
+    return Promise.reject(signal.reason);
+  }
+
+  return new Promise((resolve, reject) => {
+    const timerId = setTimeout(() => resolve(), ms);
+
+    if (signal) {
+      const handleAbort = () => {
+        clearTimeout(timerId);
+        reject(signal.reason);
+      };
+
+      signal.addEventListener('abort', handleAbort, { once: true });
+    }
+  });
 }
