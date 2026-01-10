c493f601de2c9a537f43739f806321c9b758e0c9
Dmitriy Petrov <dima.krutoo@yandex.ru>
2026-01-10 17:46:16 +0500

    feat(rspack): pluginExec now uses spawn() and has new options (#186)
    option `when` added to run script only in watch/build mode

--- src/rspack/plugin-exec.ts
+++ src/rspack/plugin-exec.ts
@@ -1,12 +1,23 @@
+/* eslint-disable no-console */
 import type { RspackPluginFunction } from '@rspack/core';
-import { exec } from 'node:child_process';
+import { type ChildProcess, spawn } from 'node:child_process';
 
 export interface PluginExecOptions {
-  /** Script to run, for example `node hello.js`. */
+  /**
+   * Script to run, for example `node hello.js`.
+   */
   script: string;
 
-  /** Need to wait for script ended or not. */
+  /**
+   * Need compilation to wait for script ended or not.
+   */
   blocking?: boolean;
+
+  /**
+   * Condition to run script depends on mode.
+   * Pass `watch` to run script only in watch mode or `build` to run not in watch mode.
+   */
+  when?: 'always' | 'build' | 'watch';
 }
 
 /**
@@ -14,23 +25,63 @@ export interface PluginExecOptions {
  * @param options Options.
  * @returns Plugin.
  */
-export function pluginExec({ script, blocking = true }: PluginExecOptions): RspackPluginFunction {
-  const pluginName = 'krutoo:pluginExec';
+export function pluginExec({
+  script,
+  blocking = true,
+  when = 'always',
+}: PluginExecOptions): RspackPluginFunction {
+  const pluginName = 'pluginExec';
 
   return compiler => {
+    if (when === 'watch' && !compiler.options.watch) {
+      return;
+    }
+
+    if (when === 'build' && compiler.options.watch) {
+      return;
+    }
+
+    let childProcess: ChildProcess | null = null;
+
+    compiler.hooks.watchRun.tap(pluginName, () => {
+      if (childProcess) {
+        childProcess.kill();
+        childProcess = null;
+        console.log(`[${pluginName}] Previous process killed`);
+      }
+    });
+
     compiler.hooks.afterEmit.tapPromise({ name: pluginName }, async compilation => {
       if (compilation.errors.length > 0) {
+        console.log(`[${pluginName}] Skip because of compilation errors`);
         return;
       }
 
       const promise = new Promise<void>((done, fail) => {
-        // @todo logger trough option, console by default
-        exec(script, error => {
-          if (error) {
-            fail(error);
-          } else {
+        childProcess = spawn(script, {
+          shell: true,
+          stdio: 'inherit',
+        });
+
+        childProcess.on('spawn', () => {
+          console.log(`[${pluginName}] Child process started, pid: ${childProcess?.pid}`);
+        });
+
+        childProcess.on('error', error => {
+          fail(error);
+        });
+
+        childProcess.on('close', (code, signal) => {
+          if (code === 0 || signal === 'SIGTERM') {
             done();
+          } else {
+            fail(new Error(`[${pluginName}] Child process closed, code ${code}`));
           }
+        });
+
+        process.on('exit', () => {
+          childProcess?.kill();
+          childProcess = null;
         });
       });
 
