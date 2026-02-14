3e7431e56a5c95b33afac57eb30915409a6356ff
krutoo <dima.krutoo@yandex.ru>
2026-02-14 16:51:43 +0500

    feat: Inline sources added to package
    TS option inlineSources enabled, js/dts files build separated

--- scripts/build.ts
+++ scripts/build.ts
@@ -2,16 +2,16 @@
  * This script will build package.
  */
 import fs from 'node:fs/promises';
-import { exec } from 'node:child_process';
-import { promisify } from 'node:util';
-
-const execAsync = promisify(exec);
+import { execAsync } from './utils.ts';
 
 // clean dist folder
 await fs.rm('./dist', { recursive: true, force: true });
 
-// build esm files
-await execAsync('npx tsc -p tsconfig.build.json');
+// build .js files
+await execAsync('npx tsc -p tsconfig.build-js.json');
+
+// build .d.ts files
+await execAsync('npx tsc -p tsconfig.build-dts.json');
 
 // format (for reduce package size by replacing indent from 4 to 2)
-await execAsync('npx prettier "dist/**/*.js" --write --ignore-path=./.nonexistent');
+await execAsync('npx prettier "dist/**/*.js" -w --log-level=error --ignore-path=./.nonexistent');
