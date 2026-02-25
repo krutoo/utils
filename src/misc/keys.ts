b5bb34e6222046726381399a103514c4cb3f6796
Dmitriy Petrov <dima.krutoo@yandex.ru>
2026-02-25 23:12:45 +0500

    perf(misc): keys optimized (#207)
    - no loop over non own keys by `Object.keys`
    - `Object.create(null)` to except prototype properties
    - `for` instead `for of` 
    - and more

--- src/misc/keys.ts
+++ src/misc/keys.ts
@@ -1,23 +1,31 @@
-// Using `Object.prototype.hasOwnProperty` instead of `Object.hasOwn` to support more environments
-const hasOwn = Object.prototype.hasOwnProperty;
-
 /**
- * Returns generator that iterates over each unique own key of all given objects.
+ * Returns generator that iterates over each unique own enumerable key of all given objects.
  * @param objects Objects to pick keys.
  * @yields Every unique own key.
  */
 export function* keys(...objects: Array<Record<string, any>>): Generator<string, void, unknown> {
-  const map: Record<string, any> = {};
+  // Object.create(null) instead Set for more efficiently memory usage on small objects
+  const seen: Record<string, boolean> = Object.create(null);
 
-  for (const item of objects) {
-    for (const key in item) {
-      if (hasOwn.call(item, key)) {
-        if (map[key]) {
-          continue;
-        } else {
-          map[key] = true;
-          yield key;
-        }
+  // "for" instead "for of" for performance (no extra iterators)
+  for (let i = 0; i < objects.length; i++) {
+    const item = objects[i];
+
+    // filter spares in objects array
+    if (!item) continue;
+
+    // Object.keys to get only own enumerable string keys and ignore spares
+    const itemKeys = Object.keys(item);
+
+    // "for" instead "for of" for performance (no extra iterators)
+    for (let j = 0; j < itemKeys.length; j++) {
+      // non-null assertion because can guarantee that key is string here
+      const key = itemKeys[j]!;
+
+      // `key in seen` instead `seen[key]` for micro optimization
+      if (!(key in seen)) {
+        seen[key] = true;
+        yield key;
       }
     }
   }
